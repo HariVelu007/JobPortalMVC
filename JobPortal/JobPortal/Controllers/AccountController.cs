8e0163c0acc74b406efbac8c9f59068949c28b51
Hari <76178534+HariVelu007@users.noreply.github.com>
2023-10-17 09:08:08 +0530

    Revert "Removed test statement"
    This reverts commit 300b054c66652ce04baec75b3406f33e3b0b11c3.

--- JobPortal/JobPortal/Controllers/AccountController.cs
+++ JobPortal/JobPortal/Controllers/AccountController.cs
@@ -39,7 +39,8 @@ namespace JobPortal.Controllers
         }
         [HttpGet]
         public IActionResult Register()
-        {            
+        {
+            TempData["StatusMessage"] = "Employer registered successfully";
             return View();
         }
         [HttpGet]
